a9a73b8cc3a91d5410ef9d17170cd375e8cdb7d7
dahuangying <127608168+dahuangying@users.noreply.github.com>
2025-05-17 01:21:22 +0800

    Update build_x86_64.sh

--- scripts/build_x86_64.sh
+++ scripts/build_x86_64.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-set -e
 
-ARCH_DIR="./ipk/x86_64"
-mkdir -p "$ARCH_DIR"
+# 清理旧环境
+rm -rf openwrt-x86
+mkdir openwrt-x86 && cd openwrt-x86
 
-echo "模拟构建 x86_64 插件..."
-echo "fake x86_64 ipk file content" > "$ARCH_DIR/luci-app-passwall_$(date +%s)_x86_64.ipk"
+# 克隆官方 SDK
+wget https://downloads.openwrt.org/releases/23.05.3/targets/x86/64/openwrt-sdk-23.05.3-x86-64_gcc-12.3.0_musl.Linux-x86_64.tar.xz
+tar -xJf openwrt-sdk-*.tar.xz
+cd openwrt-sdk-*
+
+# 配置 feeds
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" > feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2" >> feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
+
+# 更新并安装 feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 编译所有插件
+make defconfig
+make package/passwall/compile -j$(nproc)
+make package/passwall2/compile -j$(nproc)
+make package/ssr-plus/compile -j$(nproc)
+make package/luci-app-openclash/compile -j$(nproc)
+
+# 复制 .ipk 文件
+mkdir -p ../../../ipk/x86_64/
+find bin/packages/ -name "*.ipk" -exec cp {} ../../../ipk/x86_64/ \;
+
